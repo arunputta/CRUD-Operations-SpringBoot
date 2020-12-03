3a2c2ecee0772bf86e98ea30020806dd94c7faae
SAI ARCHITHA <SAI ARCHITHA@DESKTOP-N7CTISV>
2020-12-04 00:54:36 +0530

    WH-997 UPDATE RUNNER WITH LOG4J USING SLF4J

--- src/main/java/com/nt/MessageRunner.java
+++ src/main/java/com/nt/MessageRunner.java
@@ -1,15 +1,26 @@
 package com.nt;
 
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRunner implements CommandLineRunner {
 
+	private static Logger log = (Logger) LoggerFactory.getLogger(MessageRunner.class); 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		int a=10;
-		System.out.println("AAA");
+		try{
+			log.info("STARTED");
+			System.out.println("Welcome to SpringBoot Application" +a);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		log.info("End");
 	}
 
 }
